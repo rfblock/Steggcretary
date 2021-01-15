6bfac77dc67698fefe5afd41ad331edd9daf9260
Steggy <javascriptcubing999@gmail.com>
2021-01-14 21:30:48 -0500

    Init repo. Created basic forwarding functionality

---
+++ app.py
@@ -0,0 +1,24 @@
+import requests
+from flask import Flask, request
+import schedule
+from modules import forwarder
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+@app.route('/slack/events', methods=['POST'])
+def events_api():
+	### URL Verification for Events API ###
+	if request.json['type'] == 'url_verification':
+		return data
+
+	#TODO: ADD Reminders
+	#TODO: ADD PFP changer
+	#TODO: ADD github notifs
+	#TODO: ADD moderation
+	return {
+		'reaction_added': forwarder.forwarder
+	}[request.json['event']['type']](request.json)
+	
+if __name__ == '__main__':
+	app.run(port=8080)
---
+++ modules/forwarder.py
@@ -0,0 +1,34 @@
+from app import app
+import requests
+import json
+
+def forwarder(data):
+	reaction = data['event']['reaction']
+	channel = data['event']['item']['channel']
+	ts = data['event']['item']['ts']
+
+	print(ts)
+
+	if data['event']['reaction'] == 'tallyho':
+		replies = json.loads(requests.get('https://slack.com/api/conversations.replies', params={
+			'channel': channel,
+			'ts': ts
+		}, headers={
+			'Authorization': f'Bearer {app.config["SLACK_TOKEN"]}'
+		}).content.decode('utf-8'))['messages']
+
+		#TODO: Change to config instead of magic ID
+		#If bot user found OR total messages ≥ 50
+		if [x for x in replies if x['user'] == 'U01K3418E4R'] or len(replies) >= 50:
+			return '200'
+
+		parent_ts = replies[0]['thread_ts']
+
+		requests.post('https://slack.com/api/chat.postMessage', data={
+			'channel': 'stegg',
+			'text': 'you called?',
+			'thread_ts': parent_ts
+		}, headers={
+			'Authorization': f'Bearer {app.config["SLACK_TOKEN"]}'
+		})
+	return '200'
