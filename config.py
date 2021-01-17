086489853e2faf3e542228d21457d0c2c864557d
Steggy <javascriptcubing999@gmail.com>
2021-01-17 02:58:29 -0500

    Added thread support. Removed magic strings

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ import os
 
 SLACK_TOKEN = os.getenv('SLACK_TOKEN')
 SIGNING_SECRET = os.getenv('SIGNING_SECRET')
+ACTIONS_JSON = os.getenv('ACTIONS_JSON')
+BOT_UID = os.getenv('BOT_UID')
 
 DEBUG = True
--- modules/forwarder.py
+++ modules/forwarder.py
@@ -2,32 +2,36 @@ import config
 import requests
 import json
 
-def forwarder(data):
-    reaction = data['event']['reaction']
-    channel = data['event']['item']['channel']
-    ts = data['event']['item']['ts']
-
-    print(ts)
-
-    if data['event']['reaction'] == 'tallyho':
-        replies = json.loads(requests.get('https://slack.com/api/conversations.replies', params={
+def __get_replies(channel, ts):
+    #(Parent thread) Get thread list, (Thread item) Get thread item
+    return json.loads(requests.get('https://slack.com/api/conversations.replies', params={
             'channel': channel,
             'ts': ts
         }, headers={
             'Authorization': f'Bearer {config.SLACK_TOKEN}'
         }).content.decode('utf-8'))['messages']
 
-        #TODO: Change to config instead of magic ID
-        #If bot user found OR total messages ≥ 50
-        if [x for x in replies if x['user'] == 'U01K3418E4R'] or len(replies) >= 50:
-            return '200'
+def forwarder(data):
+    reaction = data['event']['reaction']
+    channel = data['event']['item']['channel']
+    ts = data['event']['item']['ts']
+
+
+    if data['event']['reaction'] == 'tallyho':
+        replies = __get_replies(channel, ts)
 
         parent_ts = ts
         if 'thread_ts' in replies[0]:
             parent_ts = replies[0]['thread_ts']
+            replies = __get_replies(channel, parent_ts)
+
+        #If bot user found OR total messages ≥ 50
+        if [x for x in replies if x['user'] == config.BOT_UID] or len(replies) >= 50:
+            return '200'
+
 
         requests.post('https://slack.com/api/chat.postMessage', data={
-            'channel': 'stegg',
+            'channel': channel,
             'text': 'you called?',
             'thread_ts': parent_ts
         }, headers={
